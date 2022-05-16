6b4257567babcd2b352df13e417cd85b83cad359
siino-xyz <b114807h@gmail.com>
2022-05-16 23:38:46 +0900

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `wp-test-ts`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
+
+export default config
